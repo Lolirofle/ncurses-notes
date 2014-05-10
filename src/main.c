7b795d4ae36d709d1850562653385451f3dd4e2a
Lolirofle <lolipopple@hotmail.com>
2014-05-10 04:15:32 +0200

    Input somewhat

--- src/main.c
+++ src/main.c
@@ -1,11 +1,63 @@
 #include <curses.h>
 #include <menu.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
 
 #define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
 #define KEY_CTRL(x)  ((x)&0x1F)
 
+#define EXIT_ALLOCATION_FAILURE 54
+#define EXIT_INPUT_ERROR 13
+
+#define INPUTBUFFER_LENGTH 512
+
+struct EntryData{
+	unsigned short priority;
+	time_t datetime;
+	time_t deadline;
+};
+
+enum ItemDataType{
+	ITEMDATATYPE_BUTTON,
+	ITEMDATATYPE_ENTRY,
+};
+
+struct ItemData{
+	enum ItemDataType type;
+	union{
+		struct EntryData entry;
+	};
+};
+
+size_t getnstr_custom(char* buffer,size_t maxLength){
+	size_t length = 0;
+	int c;
+	while((c=getch())){
+		switch(c){
+			case KEY_ENTER:
+			case '\n':
+			case '\r':
+				*buffer = '\0';
+				return length;
+			
+			default:
+				if(length<maxLength && isprint(c)){
+					*buffer++ = c;
+					++length;
+				}
+				break;
+		}
+	}
+}
+
 int main(){
+	//Initiate buffers
+	char* inputBuffer = calloc(INPUTBUFFER_LENGTH,sizeof(char));
+	if(!inputBuffer)
+		return EXIT_ALLOCATION_FAILURE;
+
 	//Initiate screen
 	initscr();
 		cbreak();
@@ -15,14 +67,10 @@ int main(){
 	keypad(stdscr,TRUE);
 
 	//Initiate items
-	unsigned int itemsLen = 5,
+	unsigned int itemsLen = 1,
 	             itemsSize = 10;
 	ITEM** items = calloc(itemsSize+1,sizeof(ITEM*));
-	items[0] = new_item("[Add...]","Add a new item to the list");
-	items[1] = new_item("Choice 1",NULL);//set_item_userptr(items[0],)
-	items[2] = new_item("Choice 2",NULL);
-	items[3] = new_item("Choice 3",NULL);
-	items[4] = new_item("Choice 4",NULL);
+	items[0] = new_item("[Add...]",NULL);
 
 	//Initiate menu
 	MENU* menu = new_menu(items);
@@ -69,21 +117,30 @@ int main(){
 				move(LINES-1,0);
 				clrtoeol();
 
-				//Write to last lines
-				mvprintw(LINES-1,0,"You have chosen item %i with name %s and description %s",
-					item_index(currentItem),
-					item_name(currentItem),
-					item_description(currentItem)
-				);
-
-				refresh();
-				pos_menu_cursor(menu);
-
 				//If first item is selected (Add...)
 				if(item_index(currentItem)==0 && itemsLen<itemsSize){
 					unpost_menu(menu);
-						//Initialize new item
-						items[itemsLen] = new_item("Added item",NULL);
+						//Get name from input
+						mvprintw(LINES-1,0,"Input:");
+						size_t inputLength = getnstr_custom(inputBuffer,INPUTBUFFER_LENGTH);
+						if(inputLength==0)
+							return EXIT_INPUT_ERROR;
+
+						char* name = malloc(inputLength);
+						memcpy(name,inputBuffer,inputLength);
+
+						//Clear last lines
+						move(LINES-1,0);
+						clrtoeol();
+
+						//Initiate new item
+						items[itemsLen] = new_item(name,NULL);
+
+						//Allocate and initate item data
+						struct EntryData* data = malloc(sizeof(struct EntryData));
+						if(!data)
+							return EXIT_ALLOCATION_FAILURE;
+						set_item_userptr(items[itemsLen],data);
 
 						//Increment length
 						++itemsLen;
@@ -91,7 +148,17 @@ int main(){
 						//Update the menu's items pointer
 						set_menu_items(menu,items);
 					post_menu(menu);
+				}else{
+					//Write selected item info to last lines
+					mvprintw(LINES-1,0,"You have chosen item %i with name %s and description %s",
+						item_index(currentItem),
+						item_name(currentItem),
+						item_description(currentItem)
+					);
 				}
+
+				refresh();
+				pos_menu_cursor(menu);
 
 			}	break;
 
@@ -107,6 +174,12 @@ int main(){
 					if(index==0)
 						break;
 
+					//Free item data
+					free(item_name(item));
+					free(item_description(item));
+					free(item_userptr(item));
+
+					//Free item
 					free_item(item);
 
 					unpost_menu(menu);
@@ -143,4 +216,8 @@ int main(){
 	free(items);
 	free_menu(menu);
 	endwin();
+
+	free(inputBuffer);
+
+	return EXIT_SUCCESS;
 }
---
+++ src/notesimplementation.h
@@ -0,0 +1,19 @@
+//Notes API
+
+struct NotesImplementation{	
+	void*(*init)();
+	void(*free)(void*);
+
+	bool(*getEntry)(const void* notesData,const struct NoteEntry* entry);
+	bool(*addEntry)(void* notesData,const struct NoteEntry* entry);
+	bool(*insertEntry)(void* notesData,size_t index,const struct NoteEntry* entry);
+	bool(*removeEntry)(void* notesData,const struct NoteEntry* entry);
+	bool(*removeEntryByIndex)(void* notesData,unsigned int index);
+	bool(*moveEntry)(void* notesData,size_t from,size_t to);
+	bool(*swapEntry)(void* notesData,size_t a,size_t b);
+
+	bool(*forAllEntries)(void* notesData,bool(*func)(struct NoteEntry*,void* userData),void* userData);
+};
+
+bool Notes_loadFile(const char* filename,const struct NotesImplementation* impl,void* out_notesData);
+bool Notes_saveFile(const char* filename,const struct NotesImplementation* impl,const void* notesData);
