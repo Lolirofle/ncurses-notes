cc3e060af15ce52186facc2331ef2941ce50c770
Lolirofle <lolipopple@hotmail.com>
2014-05-13 02:08:29 +0200

    More or less working input using forms

--- src/main.c
+++ src/main.c
@@ -75,212 +75,232 @@ static void Item_free(ITEM* item){
 	free_item(item);
 }
 
-size_t getnstr_custom(char* buffer,size_t bufferSize){
-	buffer[0] = 'T';
-	return 1;
-}
-
-static int ItemEntry_input(){
-	FIELD *field[] = {
-		new_field(1,COLS,LINES-1,0,0,0),
-		new_field(10,10,2,2,0,0),
-		NULL
-	};
-	
-	//Field options
-	for(unsigned char i=sizeof(field)/sizeof(field[0]);i-->0;){
-		set_field_back(field[i],A_UNDERLINE);//Enable underlined field
-		field_opts_off(field[i],O_AUTOSKIP); //Disable auto skipping to next field when filled up
-		field_opts_off(field[i],O_STATIC);
-		set_max_field(field[i],512);
-	}
-
-	//Initiate form
-	FORM* form = new_form(field);
-	post_form(form);
-	
-	//Refresh display
-	refresh();
-
-	//Setup form input mode
-	enum{
-		MODE_OVERLAY,
-		MODE_INSERT,
-	}mode = MODE_INSERT;
-	form_driver(form,REQ_INS_MODE);
-
-	enum{
-		ESCAPE_FALSE,
-		ESCAPE_TRUE,
-		ESCAPE_CSI,
-	}escaped = ESCAPE_FALSE;
-	#define ESCAPEBUFFER_SIZE 16
-	char escapeBuffer[ESCAPEBUFFER_SIZE];
-	unsigned char escapeBufferLength = 0;
-
-	//Fetch input
-	int ch;
-	while((ch = getch())){
-		//http://en.wikipedia.org/wiki/ANSI_escape_code
-		if(escaped == ESCAPE_TRUE){
-			switch(ch){
-				case '[':
-					escaped = ESCAPE_CSI;
-					break;
-
-				case 0  ... 63 :
-				case 96 ... 255:
-					//TODO: Multi character escape
-					break;
-
-				default:
-				EscapeNormal_break:
-					escaped = ESCAPE_FALSE;
-					escapeBufferLength = 0;
-					break;
-			}
-		}else if(escaped ==ESCAPE_CSI){
-			switch(ch){
-				case 'D'://Word left
-					form_driver(form,REQ_PREV_WORD);
-					goto EscapeCsi_break;
-
-				case 'C'://Word right
-					form_driver(form,REQ_NEXT_WORD);
-					goto EscapeCsi_break;
-
-				case 0  ... 63 :
-				case 127 ... 255:
-					//TODO: Multi character escape
-					break;
-
-				default:
-				EscapeCsi_break:
-					escaped = ESCAPE_FALSE;
-					escapeBufferLength = 0;
-					break;
-			}
-		}else{
-			switch(ch){
-				case 9://Tab
-					form_driver(form,REQ_NEXT_FIELD);
-					break;
-
-				case 353://Shift + Tab
-					form_driver(form,REQ_PREV_FIELD);
-					break;
-
-				case KEY_DOWN:
-					form_driver(form,REQ_NEXT_LINE);
-					break;
-
-				case KEY_UP:
-					form_driver(form,REQ_PREV_LINE);
-					break;
-
-				case KEY_LEFT:
-					form_driver(form,REQ_PREV_CHAR);
-					break;
-
-				case KEY_RIGHT:
-					form_driver(form,REQ_NEXT_CHAR);
-					break;
-
-				case 262://Home
-					form_driver(form,REQ_BEG_LINE);
-					break;
-
-				case 360://End
-					form_driver(form,REQ_END_LINE);
-					break;
-
-				case KEY_BACKSPACE:
-					form_driver(form,REQ_DEL_PREV);
-					break;
-
-				case 8://CTRL + Backspace (Why is this assigned to the ASCII BS/backspace on my current setup?)
-					form_driver(form,REQ_DEL_WORD);
-					break;
-
-				case KEY_DC://Delete
-					form_driver(form,REQ_DEL_CHAR);
-					break;
-
-				case 27://Escape
-					escaped = ESCAPE_TRUE;
-					break;
-
-				case 155://CSI Escape
-					escaped = ESCAPE_CSI;
-					break;
-
-				case KEY_ENTER:
-				case 10:
-				case 13:
-					goto Form_free;
-
-				case 331://Insert
-					switch(mode){
-						case MODE_OVERLAY:
-							form_driver(form,REQ_INS_MODE);
-							mode = MODE_INSERT;
-							break;
-
-						case MODE_INSERT:
-							form_driver(form,REQ_OVL_MODE);
-							mode = MODE_OVERLAY;
-							break;
-					}
-					break;
-
-				default:
-					form_driver(form,ch);
-					break;
-			}
-		}
-	}
-
-	//Free
-	Form_free:{
-		int returnCode = EXIT_SUCCESS;
-		
-		if(form_driver(form,REQ_VALIDATION)==E_OK){
-			char* buffer = field_buffer(field,0);
-		}else
-			returnCode = EXIT_FAILURE;
-
-		unpost_form(form);
-		free_form(form);
-		for(unsigned char i=sizeof(field)/sizeof(field[0]);i-->0;)
-			free_field(field[i]);
-	}
-}
-
 static int button_performAdd(struct MenuData* menuData){
 	if(menuData->itemsLen<menuData->itemsSize){
-		size_t inputLength = getnstr_custom(inputBuffer,INPUTBUFFER_LENGTH);
+		//Initate form fields
+		FIELD* formFields[] = {
+			new_field(1,COLS-2,2,1,0,0),
+			new_field(10,COLS-2,5,1,0,0),
+			NULL
+		};
+		
+		//Field options
+		for(unsigned char i=sizeof(formFields)/sizeof(formFields[0]);i-->0;){
+			set_field_back(formFields[i],A_UNDERLINE);//Enable underlined formFields
+			field_opts_off(formFields[i],O_AUTOSKIP); //Disable auto skipping to next formFields when filled up
+			field_opts_off(formFields[i],O_STATIC);
+			set_max_field(formFields[i],512);
+		}
 
-		//Silently ignore empty input
-		if(inputLength==0)
-			return EXIT_SUCCESS;
+		//Initiate form
+		FORM* form = new_form(formFields);
+		post_form(form);
+		
+		//Refresh display
+		refresh();
 
-		inputBuffer[inputLength++] = '\0';
-		char* title = malloc(inputLength);
-		if(!title)
-			return EXIT_ALLOCATION_FAILURE;
-		memcpy(title,inputBuffer,inputLength);
+		//Print fields text
+		mvprintw(1,1,"Title:");
+		mvprintw(4,1,"Description:");
 
-		unpost_menu(menuData->menu);
-			//Initiate new item
-			if(!(menuData->items[menuData->itemsLen] = Item_createEntry(title,NULL)))
-				return EXIT_ITEMENTRYCREATION_FAILURE;
+		//Setup form input mode
+		enum{
+			MODE_OVERLAY,
+			MODE_INSERT,
+		}mode = MODE_INSERT;
+		form_driver(form,REQ_INS_MODE);
 
-			//Increment length
-			++menuData->itemsLen;
+		enum{
+			ESCAPE_FALSE,
+			ESCAPE_TRUE,
+			ESCAPE_CSI,
+		}escaped = ESCAPE_FALSE;
+		#define ESCAPEBUFFER_SIZE 16
+		char escapeBuffer[ESCAPEBUFFER_SIZE];
+		unsigned char escapeBufferLength = 0;
 
-			//Update the menu's items pointer
-			set_menu_items(menuData->menu,menuData->items);
-		post_menu(menuData->menu);
+		//Fetch input
+		int ch;
+		while((ch = getch())){
+			//http://en.wikipedia.org/wiki/ANSI_escape_code
+			if(escaped == ESCAPE_TRUE){
+				switch(ch){
+					case '['://TODO: These will match every '['
+						escaped = ESCAPE_CSI;
+						break;
+
+					case 0  ... 63 :
+					case 96 ... 255:
+						escapeBuffer[escapeBufferLength++] = ch;
+
+						if(escapeBufferLength>=ESCAPEBUFFER_SIZE)
+							goto EscapeNormal_break;
+						break;
+
+					default:
+					EscapeNormal_break:
+						escaped = ESCAPE_FALSE;
+						escapeBufferLength = 0;
+						break;
+				}
+			}else if(escaped ==ESCAPE_CSI){
+				switch(ch){
+					case 'D'://Word left
+						form_driver(form,REQ_PREV_WORD);
+						goto EscapeCsi_break;
+
+					case 'C'://Word right
+						form_driver(form,REQ_NEXT_WORD);
+						goto EscapeCsi_break;
+
+					case 0  ... 63 :
+					case 127 ... 255:
+						escapeBuffer[escapeBufferLength++] = ch;
+						
+						if(escapeBufferLength>=ESCAPEBUFFER_SIZE)
+							goto EscapeCsi_break;
+						break;
+
+					default:
+					EscapeCsi_break:
+						escaped = ESCAPE_FALSE;
+						escapeBufferLength = 0;
+						break;
+				}
+			}else{
+				switch(ch){
+					case 9://Tab
+						form_driver(form,REQ_NEXT_FIELD);
+						break;
+
+					case 353://Shift + Tab
+						form_driver(form,REQ_PREV_FIELD);
+						break;
+
+					case KEY_DOWN:
+						form_driver(form,REQ_NEXT_LINE);
+						break;
+
+					case KEY_UP:
+						form_driver(form,REQ_PREV_LINE);
+						break;
+
+					case KEY_LEFT:
+						form_driver(form,REQ_PREV_CHAR);
+						break;
+
+					case KEY_RIGHT:
+						form_driver(form,REQ_NEXT_CHAR);
+						break;
+
+					case 262://Home
+						form_driver(form,REQ_BEG_LINE);
+						break;
+
+					case 360://End
+						form_driver(form,REQ_END_LINE);
+						break;
+
+					case KEY_BACKSPACE:
+						form_driver(form,REQ_DEL_PREV);
+						break;
+
+					case 8://CTRL + Backspace (Why is this assigned to the ASCII BS/backspace on my current setup?)
+						form_driver(form,REQ_DEL_WORD);
+						break;
+
+					case KEY_DC://Delete
+						form_driver(form,REQ_DEL_CHAR);
+						break;
+
+					case 27://Escape
+						escaped = ESCAPE_TRUE;
+						break;
+
+					case 155://CSI Escape
+						escaped = ESCAPE_CSI;
+						break;
+
+					case KEY_ENTER:
+					case 10:
+					case 13:
+						if(form_driver(form,REQ_VALIDATION)==E_OK)
+							goto Form_complete;
+						break;
+
+					case 331://Insert
+						switch(mode){
+							case MODE_OVERLAY:
+								form_driver(form,REQ_INS_MODE);
+								mode = MODE_INSERT;
+								break;
+
+							case MODE_INSERT:
+								form_driver(form,REQ_OVL_MODE);
+								mode = MODE_OVERLAY;
+								break;
+						}
+						break;
+
+					default:
+						form_driver(form,ch);
+						break;
+				}
+			}
+		}
+
+		Form_complete:{
+			char* title       = field_buffer(formFields[0],0),
+			    * description = field_buffer(formFields[1],0);
+			size_t titleLen       = strlen(title)+1,
+			       descriptionLen = strlen(description)+1;
+
+			//Silently ignore empty input
+			//if(titleLen == 0)
+			//	return EXIT_SUCCESS;
+
+			//Copy over buffer to newly allocated block
+			char* titleStr,
+			    * descriptionStr;
+
+			if(titleLen == 1)
+				titleStr = NULL;
+			else{
+				if(!(titleStr = malloc(titleLen)))
+					return EXIT_ALLOCATION_FAILURE;
+				memcpy(titleStr,title,titleLen);
+			}
+
+			if(descriptionLen == 1)
+				descriptionStr = NULL;
+			else{
+				if(!(descriptionStr = malloc(descriptionLen)))
+					return EXIT_ALLOCATION_FAILURE;
+				memcpy(descriptionStr,description,descriptionLen);	
+			}
+
+			//Free form and its fields
+			unpost_form(form);
+			free_form(form);
+			for(unsigned char i=sizeof(formFields)/sizeof(formFields[0]);i-->0;)
+				free_field(formFields[i]);
+
+			//Update menu with a new entry
+			unpost_menu(menuData->menu);
+				//Initiate new item
+				if(!(menuData->items[menuData->itemsLen] = Item_createEntry(titleStr,descriptionStr)))
+					return EXIT_ITEMENTRYCREATION_FAILURE;
+
+				//Increment length
+				++menuData->itemsLen;
+
+				//Update the menu's items pointer
+				set_menu_items(menuData->menu,menuData->items);
+			post_menu(menuData->menu);
+			
+			//Refresh display
+			refresh();
+		}
 	}
 
 	return EXIT_SUCCESS;
